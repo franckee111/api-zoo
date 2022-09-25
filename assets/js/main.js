bce3aaf999a119367e8a9f1f5d136575f63badb8
franckee111 <frestrada.dwfsc9@ucamp.utel.edu.mx>
2022-09-25 14:07:59 -0500

    cargar nueva gráfica

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,9 @@ const base_url_api = "http://ucamp.alumnos.dev4humans.com.mx";
 const tblAnimales = document.getElementById('tblAnimales');
 const animales = "Main/endpoint_animales_zoo";
 const url = `http://ucamp.alumnos.dev4humans.com.mx/${animales}`;
+const grafica  = document.getElementById('myChart').getContext('2d');
+const loadContainer = document.getElementById("loaderContainer");
+const rowContainer = document.getElementById("rowContainer");
 
 // fetch(url_api)
 //     .then((response) => {
@@ -14,13 +17,67 @@ const url = `http://ucamp.alumnos.dev4humans.com.mx/${animales}`;
 //     .then((data) => console.log(data))
 //     .catch((error) => console.log(error))
 
-function cargarAnimal(base_url_api) { 
+const labels = Utils.months({count: 7});
+const myChart = new Chart(grafica, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Cantidad de animales',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: true,
+                borderColor: 'rgb(124, 198, 238)',
+                tension: 0.1
+            }
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+function cargarAnimal(base_url_api) {
     fetch(url, { method: "GET" })
         .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
+        .then((result) => {
+            // console.log(result);
+            // console.log(result.data.map(item => item.nombre));
+            // console.log(result.data.map(item => item.cantidad));
+
+            const labels_for_chart = result.data.map(item => item.nombre);
+            const data_for_chart = result.data.map(item => item.cantidad);
+
+            const myChart = new Chart(grafica, {
+                type: 'line',
+                data: {
+                    labels: labels_for_chart,
+                    datasets: [
+                        {
+                            label: 'Cantidad de animales',
+                            data: data_for_chart,
+                            fill: true,
+                            backgroundColor: '#ccd9ff',
+                            borderColor: 'rgb(124, 198, 238)',
+                            tension: 0.1
+                        }
+                    ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
             tblAnimales.innerHTML = ""; // limpiar registros
-            for (const animal of data.data) {
+            for (const animal of result.data) {
                 let tr = `<tr>
                         <td> ${animal.id} </td>    
                         <td> ${animal.nombre} </td>
@@ -28,9 +85,12 @@ function cargarAnimal(base_url_api) {
                     </tr>`;
                 tblAnimales.innerHTML += tr;
             }
-            if (data.lenght == 0) {
+            if (result.lenght == 0) {
                 tblAnimales.innerHTML = `<tr> <td colspan="5" class="text-center">No hay animales registrados</td></tr>`;
-            } 
+            }
+
+            loaderContainer.classList.add("d-none");
+            rowContainer.classList.remove("d-none");
         })
         .catch((error) => console.log(error));
 }
